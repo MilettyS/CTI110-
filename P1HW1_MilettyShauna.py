dbd6dba265a5a993394ba3b2e8f2305ee8f79426
MilettyS <mcleans5695@student.faytechcc.edu>
2025-04-05 22:32:32 -0400

    Add files via upload

---
+++ P1HW1_MilettyShauna.py
@@ -0,0 +1,26 @@
+#Shauna Miletty
+#04/05/2025
+#PH1HW1
+#using print and input to calculate intergers
+
+print("-----Calculating Exponents----")
+print("\n")
+
+#Getting base and exponet values from user
+base=int(input("Enter an integer as base value: "))
+exponent=int(input("Enter an interger as exponent: "))
+
+#calculate and display the result
+print("\n")
+print(base, "raised to the power of", exponent, "is", base**exponent, "!")
+
+print("-----Addition and Subraction------")
+print("\n")
+
+#get three integers from the user for calculating
+num1=int(input("Enter a starting interger: "))
+num2=int(input("Enter an integer to add "))
+num3=int(input("Enter an ineger to subract: "))
+
+print("\n")
+print("num1 + num2 - num3 is equal to sum")
